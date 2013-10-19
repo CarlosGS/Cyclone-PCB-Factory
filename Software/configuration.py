0591038750e689dde251de15e98d3597ed9f902a
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2013-10-19 12:38:23 +0200

    Some more milling

--- Software/configuration.py
+++ Software/configuration.py
@@ -1,11 +1,11 @@
 # Begin configuration
 BAUDRATE = 115200
 DEVICE = "/dev/ttyUSB0"
-Emulate = True # Won't connect to the machine, will emulate the commands!
+Emulate = False # Won't connect to the machine, will emulate the commands!
 
 # For GenerateGcode and Send
 filePath = "./GcodeGenerators/pyGerber2Gcode_CUI/out/"
-fileName = "GNBoard"
+fileName = "4LDR_Board"
 # Note: Don't forget to edit ./GcodeGenerators/pyGerber2Gcode_CUI/pygerber2gcode_cui_MOD.conf to match the name and board files
 
 # For Zprobe
@@ -17,8 +17,8 @@ F_fastMove = 70000 # mm/s
 F_slowMove = 20000 # mm/s
 initial_Z_lowering_distance = -5 # Warning: Do not lower too much or you will potentially cause damage!
 
-N_copies_X = 2 # Panelizing options!
-N_copies_Y = 2
+N_copies_X = 1 # Panelizing options!
+N_copies_Y = 1
 margin_copies_X = 5 # mm
 margin_copies_Y = 5 # mm
 
@@ -31,7 +31,7 @@ showDrill=0
 showEdge=0 # Caution, buggy!
 
 Zlift_milling = 1.0 # mm
-Z_global_offset = 0 #-0.018 go deeper!
+Z_global_offset = -0.02 #-0.018 go deeper!
 
 maxDistance = 1**2 # [mm^2] 2mm (longer moves will be split to regulate Z)
 minDistance = 0.001**2 # [mm^2] 0.001mm is the smallest distance that will be sent
